a7d50acc1b316fe6aaf5e922bc26da72b6583435
chrisco210 <chrisrachlinski@gmail.com>
2018-08-24 09:51:12 -0400

    Cleanup code, add readme

--- TempHum_Shutdown/TempHum_Shutdown.ino
+++ TempHum_Shutdown/TempHum_Shutdown.ino
@@ -1,15 +1,13 @@
 #include <OneWire.h>
-#define SHUTDOWN_PIN 23
-#define DS18S20_Pin 17
 #include <U8g2lib.h>
 #include <lmic.h>
 #include <hal/hal.h>
 #include <SPI.h>
 
+//Use these to set the shutdown output pin and the temp sensor pin
+#define SHUTDOWN_PIN 23
+#define DS18S20_Pin 17
 
-#define BATPIN A7     //Battery level pin
-#define ACTIVEPIN 11   //Activity LED pin
-#define DHTPIN 10    // what pin we're connected to
 
 //# of bytes in the payload
 #define PAYLOAD_SIZE 4
@@ -39,20 +37,9 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-// USE THIS TO CHANGE INTERVAL
 const unsigned TX_INTERVAL = 300;   //5min
 
 // Pin mapping
-/*
-//Feather M0
-const lmic_pinmap lmic_pins = {
-    .nss = 8,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = 4,
-    .dio = {3, 5, 6},
-};
-*/
-
 //Heltec esp32
 const lmic_pinmap lmic_pins = {
     .nss = 18,
@@ -64,85 +51,45 @@ const lmic_pinmap lmic_pins = {
 unsigned long startTime;
 //Temperature chip i/o
 OneWire ds(DS18S20_Pin);  // on digital pin 2
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
 
 void setup(void) {
-  //digitalWrite(SHUTDOWN_PIN, LOW);
-  //pinMode(SHUTDOWN_PIN, OUTPUT);
-  
+  data = (uint8_t*) calloc(PAYLOAD_SIZE, sizeof(uint8_t));    //Allocate the required number of bytes for the payload
 
-  
-  Serial.begin(115200);
-    
-    
-    Serial.println("Starting");
-    
-    startTime = millis();
-    u8x8.begin();
-  u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0, 0, "Starting");
-            // put your setup code here, to r un once:
-    
-    data = (uint8_t*) calloc(PAYLOAD_SIZE, sizeof(uint8_t));    //Allocate the required number of bytes for the payload
+  // LMIC init
+  os_init();
 
-    Serial.println("os_init");
-    u8x8.drawString(0, 0, "os_init");
-    // LMIC init
-    os_init();
-    Serial.println("LMIC_reset");
-    u8x8.drawString(0, 0, "LMIC reset");
-    // Reset the MAC state. Session and pending data transfers will be discarded.
-    LMIC_reset();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
 
-    Serial.println("setLinkCheckMode");
-    u8x8.drawString(0, 0, "setLinkCheckMode");
-    LMIC_setLinkCheckMode(0);
-    Serial.println("setDrTxpow");   
-    u8x8.drawString(0, 0, "setDrTxpow");
-    LMIC_setDrTxpow(DR_SF7,14);
-    Serial.println("selectSubBand");
-    u8x8.drawString(0, 0, "selectSubBand");
-    LMIC_selectSubBand(1);
+  LMIC_setLinkCheckMode(0);
+  LMIC_setDrTxpow(DR_SF7,14);
+  LMIC_selectSubBand(1);
 
-    Serial.println("do_send");
-    u8x8.drawString(0, 0, "do_send");
-    // Start job (sending automatically starts OTAA too)    
-    do_send(&sendjob);
-    
+  do_send(&sendjob);  
 }
 
 void loop(void) {
   os_runloop_once();
-  
-  
 }
 
+
+//Event handler
 void onEvent (ev_t ev) {
-    if(ev == EV_TXCOMPLETE) {
-      u8x8.drawString(0, 0, "EV_TXCOMPLETE");
-      powerDown();
-    } else if(ev == EV_JOINED ) {
-            Serial.println(F("EV_JOINED"));
-            u8x8.drawString(0, 0, "EV_JOINED");
-            u4_t netid = 0;
-            devaddr_t devaddr = 0;
-            u1_t nwkKey[16];
-            u1_t artKey[16];
-            LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
-            // Disable link check validation (automatically enabled
-            // during join, but because slow data rates change max TX
-            // size, we don't use it in this example.
-            LMIC_setLinkCheckMode(0);
+  //Transfer is complete, shutdown
+  if(ev == EV_TXCOMPLETE) {
+    powerDown();
+  } else if(ev == EV_JOINED ) {   //Joined, get OTAA keys
+    u4_t netid = 0;
+    devaddr_t devaddr = 0;
+    u1_t nwkKey[16];
+    u1_t artKey[16];
+    LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
+    LMIC_setLinkCheckMode(0);
   }
 }
 
+//Send power down signal
 void powerDown() {
-  
-  
-  Serial.println(millis() - startTime);
-  Serial.println("Finished");
-  u8x8.drawString(0, 0, "Finished. Shutdown");
-  
   digitalWrite(SHUTDOWN_PIN, LOW);
   pinMode(SHUTDOWN_PIN, OUTPUT);
 }
@@ -150,60 +97,44 @@ void powerDown() {
  * This function actually sends the data to the gateway
  */
 void do_send(osjob_t* j){
-// Check if there is not a current TX/RX job running
-    Serial.println("Checking");
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-        u8x8.drawString(0, 0, "OP_TXRXPEND, no send");
-    } else {
-        Serial.println("Collecting data");
-        u8x8.drawString(0, 0, "Collecting Data");
-        // Prepare upstream data transmission at the next possible time.
-        for(int i = 0; i < 30; i++) {
-          collectData();
-        }
-        
-
-          Serial.println("Sending");
-          u8x8.drawString(0, 0, "Sending");
-  LMIC_setTxData2(1, data, PAYLOAD_SIZE, 0);   //PAYLOAD_SIZE bytes being sent
-        Serial.println(F("Packet queued"));
-        u8x8.drawString(0, 0, "Packet Queued");
+  if (!(LMIC.opmode & OP_TXRXPEND)) {
+    Serial.println("Collecting data");
+    u8x8.drawString(0, 0, "Collecting Data");
+    
+    //Collecting data 30 times, this ensures that the sensor is found and reads a proper value
+    for(int i = 0; i < 30; i++) {
+      collectData();
     }
-
-  
-  
+    
+    LMIC_setTxData2(1, data, PAYLOAD_SIZE, 0);
+  }
 }
 
 /*
  * Collect data from sensors and battery and place it in the output pointer to be sent
  */
 void collectData() {
-  //int temp = (int) getTemp();
-  //memcpy((void*) data, (void*) &temp, sizeof(int));
-  
-  
-  float2bytes(getTemp(), (void*) (data));
+  int2bytes(getTemp(), (void*) (data));
 }
 
 /*
  * Convert a floating point number to 4 bytes at the output location provided
  * Uses memcopy, so don't have the output location be the same as the location of the input float
  */
-void float2bytes(int input, void* outputLocation) {
+void int2bytes(int input, void* outputLocation) {
   memcpy(outputLocation, (void*) &input, sizeof(int));
 }
 
 //From sensor library example, am not touching
-int getTemp(){
+int getTemp() {
   //returns the temperature from one DS18S20 in DEG Celsius
   byte data[12];
   byte addr[8];
 
-  if ( !ds.search(addr)) {
-      //no more sensors on chain, reset search
-      ds.reset_search();
-      return -1000;
+  if(!ds.search(addr)) {
+    //no more sensors on chain, reset search
+    ds.reset_search();
+    return -1000;
   }
 
   ds.reset();
@@ -215,7 +146,7 @@ int getTemp(){
   ds.write(0xBE); // Read Scratchpad
 
   
-  for (int i = 0; i < 9; i++) { // we need 9 bytes
+  for(int i = 0; i < 9; i++) { // we need 9 bytes
     data[i] = ds.read();
   }
   
@@ -224,7 +155,7 @@ int getTemp(){
   byte MSB = data[1];
   byte LSB = data[0];
 
-  float tempRead = (int16_t) ((MSB << 8) | LSB); //using two's compliment
+  float tempRead = (int16_t) ((MSB << 8) | LSB);
   float TemperatureSum = tempRead / 16;
 
   Serial.print("TEMP: ");
